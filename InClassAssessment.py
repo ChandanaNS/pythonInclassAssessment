3aad69d1c28f16369a3442ca7324ad9e29716f9d
ChandanaNS <10539269@mydbs.ie>
2020-03-24 01:22:29 +0000

    Github repo update

--- InClassAssessment.py
+++ InClassAssessment.py
@@ -1,3 +1,7 @@
-# Inclass assessment
-# Date: 24-03-20
-# Authour: Chandana
+'''
+description: Inclass assessment
+Date: 24-03-20
+Authour: Chandana
+@gitHub url: https://github.com/ChandanaNS/pythonInclassAssessment
+'''
+
